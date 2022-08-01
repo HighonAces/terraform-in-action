7383a6fc251031e8e25fbe80674890a7f68f0bd0
HighonAces <srjnreddy33@gmail.com>
2022-08-01 13:07:07 -0500

    modified EOT

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version = ">= 0.15"
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "~> 2.0"
-    }
+required_version = ">= 0.15"
+required_providers {
+  local = {
+    source  = "hashicorp/local"
+    version = "~> 2.0"
   }
+}
 }
 
 resource "local_file" "literature" {
-  filename = "art_of_war.txt"
-  <<-EOT
-    Sun Tzu said: The art of war is of vital importance to the State.
+filename = "art_of_war.txt"
+content = <<-EOT
+      Sun Tzu said: The art of war is of vital importance to the State.
 
-    It is a matter of life and death, a road either to safety or to
-    ruin. Hence it is a subject of inquiry which can on no account be
-    neglected.
-  EOT
+      It is a matter of life and death, a road either to safety or to
+      ruin. Hence it is a subject of inquiry which can on no account be
+      neglected.
+    EOT
 }
