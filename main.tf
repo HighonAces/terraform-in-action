0a6e82c0bdb3ab3a84d078bcab91f77b5cd4ce34
HighonAces <srjnreddy33@gmail.com>
2022-08-01 11:46:38 -0500

    changed ami

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "hello-world" {
-  ami = "ami-0b0950360cb811cfc"
+  ami = "ami-09dd2e08d601bff67"
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
