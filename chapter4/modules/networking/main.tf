6dc7265ea9810ce5480a26704b2936c8efc18173
HighonAces <srjnreddy33@gmail.com>
2022-08-02 12:20:45 -0500

    changed cidr

--- chapter4/modules/networking/main.tf
+++ chapter4/modules/networking/main.tf
@@ -4,7 +4,8 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "2.64.0"
   name = "${var.namespace}-vpc"
-  cidr = data.aws_availability_zones.available.names
+  cidr = "10.0.0.0/16"
+  azs = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   database_subnets                 = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
