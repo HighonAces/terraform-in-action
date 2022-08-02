a2e356e1b0bff3f408bfcc58bf5a5080a3bac317
HighonAces <srjnreddy33@gmail.com>
2022-08-02 14:45:55 -0500

    added package url

--- chapter5/main.tf
+++ chapter5/main.tf
@@ -70,6 +70,10 @@ data "azurerm_storage_account_sas" "storage_sas" {
   }
 }
 
+locals {
+  package_url = "https://${azurerm_storage_account.storage_account.name}.blob.core.windows.net/${azurerm_storage_container.storage_container.name}/${azurerm_storage_blob.storage_blob.name}${data.azurerm_storage_account_sas.storage_sas.sas}"
+}
+
 resource "azurerm_app_service_plan" "plan" {
   location            = azurerm_resource_group.default.location
   name                = local.namespace
