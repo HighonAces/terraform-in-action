f9dbc4e19af347981ccc11075be2b83e3a7397c6
HighonAces <srjnreddy33@gmail.com>
2022-08-03 12:02:47 -0500

    added test.tf

---
+++ test/test.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "s3backend-hsusdryfhjhm0f-state-bucket"
+    key = "jesse/james"
+    region = "us-west-2"
+    encrypt = true
+    role_arn = "arn:aws:iam::473189215807:role/s3backend-hsusdryfhjhm0f-tf-assume-role"
+    dynamodb_table = "s3backend-hsusdryfhjhm0f-state-lock"
+  }
+  required_version = ">= 0.15"
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
